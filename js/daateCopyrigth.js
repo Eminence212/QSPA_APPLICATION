cd9dbfc40a218622e168fc31cafc542ca88ab2b7
Eminence <mulemanowa@gmail.com>
2023-08-18 19:11:35 +0100

    initialisation

---
+++ js/daateCopyrigth.js
@@ -0,0 +1,5 @@
+function todayDate() {
+  const dateString =
+    "Créer par meshchak KUFAKWINGI © " + new Date().getFullYear() + "  ";
+  return (document.getElementById("date").innerHTML = dateString);
+}
